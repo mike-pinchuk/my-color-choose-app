5681a6736f9d076bfaf05ac660ac81d50a4fa75b
Mike <morning.sunshine.work@gmail.com>
2020-08-04 23:00:40 +0300

    Updates

---
+++ precache-manifest.24d9078300c6fe13e872d7f21b497924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32b668cef05f62e728f35e5a0c20846",
+    "url": "/movie-list-react-app/index.html"
+  },
+  {
+    "revision": "cfec2b2492e067354e70",
+    "url": "/movie-list-react-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "600f240b72c5f76349c0",
+    "url": "/movie-list-react-app/static/css/main.a1308ec3.chunk.css"
+  },
+  {
+    "revision": "cfec2b2492e067354e70",
+    "url": "/movie-list-react-app/static/js/2.f1ca4a07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-list-react-app/static/js/2.f1ca4a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600f240b72c5f76349c0",
+    "url": "/movie-list-react-app/static/js/main.955acbdb.chunk.js"
+  },
+  {
+    "revision": "73052b2b7487f2d5c895",
+    "url": "/movie-list-react-app/static/js/runtime-main.b9f7f957.js"
+  }
+]);
